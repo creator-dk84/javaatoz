44e5303e490aed547b20a63872a4ac089b1a9a27
creator-dk84 <deukhee.kim@datadoghq.com>
2024-08-04 17:00:11 +0900

    Create Main Thread

---
+++ src/test/java/com/example/javaatoz/JavaatozApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaatoz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaatozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
